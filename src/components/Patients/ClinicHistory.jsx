4846f1219b29f6c4d870ce4b057e2349b3a6121b
Ricardo Del Rio <rdelrio.ds@gmail.com>
2022-06-25 02:41:44 -0500

    fix - prevent error when no clinicHistory data is given

--- src/components/Patients/ClinicHistory.jsx
+++ src/components/Patients/ClinicHistory.jsx
@@ -8,31 +8,37 @@ function ClinicHistory(props) {
 
 	return (
 		<div className="clinicHistoryC">
-			<fieldset>
-				<legend>Historia Clinica</legend>
-				<div className="clinicHistoryGrid">
-					<div className="clinicHistoryGridLeft">
-						<b>Motivo de la consulta:</b>
-						<p>{data.reason === "" ? "-" : data.reason}</p>
-						<b>Enfermedades</b>
-						<p>{data.conditions === "" ? "-" : data.conditions}</p>
-						<b>Malestares</b>
-						<p>{data.illness === "" ? "-" : data.illness}</p>
-						<b>Alergias</b>
-						<p>{data.allergies === "" ? "-" : data.allergies}</p>
+			{data === null ? (
+				<></>
+			) : (
+				<fieldset>
+					<legend>Historia Clinica</legend>
+					<div className="clinicHistoryGrid">
+						<div className="clinicHistoryGridLeft">
+							<b>Motivo de la consulta:</b>
+							<p>{data.reason === "" ? "-" : data.reason}</p>
+							<b>Enfermedades</b>
+							<p>{data.conditions === "" ? "-" : data.conditions}</p>
+							<b>Malestares</b>
+							<p>{data.illness === "" ? "-" : data.illness}</p>
+							<b>Alergias</b>
+							<p>{data.allergies === "" ? "-" : data.allergies}</p>
+						</div>
+						<div className="clinicHistoryGridRight">
+							<b>No incluir en la dieta</b>
+							<p>{data.notIncluded === "" ? "-" : data.notIncluded}</p>
+							<b>Ingesta de agua</b>
+							<p>
+								{data.waterConsumption === "" ? "-" : data.waterConsumption}
+							</p>
+							<b>Ejercicio</b>
+							<p>{data.exercise === "" ? "-" : data.exercise}</p>
+							<b>Observaciones:</b>
+							<p>{data.comments === "" ? "-" : data.comments}</p>
+						</div>
 					</div>
-					<div className="clinicHistoryGridRight">
-						<b>No incluir en la dieta</b>
-						<p>{data.notIncluded === "" ? "-" : data.notIncluded}</p>
-						<b>Ingesta de agua</b>
-						<p>{data.waterConsumption === "" ? "-" : data.waterConsumption}</p>
-						<b>Ejercicio</b>
-						<p>{data.exercise === "" ? "-" : data.exercise}</p>
-						<b>Observaciones:</b>
-						<p>{data.comments === "" ? "-" : data.comments}</p>
-					</div>
-				</div>
-			</fieldset>
+				</fieldset>
+			)}
 		</div>
 	);
 }
